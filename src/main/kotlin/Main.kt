2053d4baba3fdfd849e48142a5b0c78e3b195113
Sharonanota <matakwamatakwa@gmail.com>
2022-02-16 20:42:17 +0300

    anota

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var name:String=( "Sharon Lubembe")
+    println(name)
+    var age =22
+    println(age)
+    val phoneNumber = "0722939046"
+    println(phoneNumber)
+    var weight = 62
+    println(weight)
+    var Kenyan: Boolean = true >false
+    println(Kenyan)
+}
